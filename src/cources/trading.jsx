067ae7f073c305ccd27b8932d9fc560ec196dd3f
Hellsrest <bishampandey100@gmail.com>
2024-11-22 14:42:04 +0545

    merge-fix

--- src/cources/trading.jsx
+++ src/cources/trading.jsx
@@ -1,12 +1,9 @@
 import React from "react";
-import "./banking.css";
+
 const StockTradingBasics = () => {
   return (
     <div className="banking-info-container">
       <h1 className="title">Stock Trading: A Beginner's Guide</h1>
-    <div className="stock-trading-container">
-      <h1>Stock Trading: A Beginner's Guide</h1>
-      <img src="/trading.jpg" alt="bishem_gay" style={{height:"50vh", display:"block", justifySelf:"center"}} />
       <section>
         <h2>What is Stock Trading?</h2>
         <p className="intro">
